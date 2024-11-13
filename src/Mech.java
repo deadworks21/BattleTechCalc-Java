36f93a40362a738e6a1df5a065a42e027b3c417a
Fretwell <RFRETWEL@depaul.edu>
2024-11-13 16:48:25 -0600

    Initial commit

---
+++ src/Mech.java
@@ -0,0 +1,30 @@
+public class Mech {
+    private String name;
+    private Integer tonnage;
+    private Integer heat;
+
+    public Mech(String name,int tonnage,int heat){
+        this.name = name;
+        this.tonnage = tonnage;
+        this.heat = heat;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getTons(){
+        return tonnage;
+    }
+
+    public int getHeat(){
+        return heat;
+    }
+
+    public void getInfo(){
+        System.out.println("Mech Name" + name);
+        System.out.println("Mech Tonnage" + tonnage);
+        System.out.println("Current Heat" + heat);
+    }
+
+}
